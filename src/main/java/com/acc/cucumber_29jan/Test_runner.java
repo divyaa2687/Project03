28cd2d85cc60385332c4d46841bfaadf148a3531
divyaa2687 <60376648+divyaa2687@users.noreply.github.com>
2020-01-29 13:35:14 +0530

    hjkl

--- src/main/java/com/acc/cucumber_29jan/Test_runner.java
+++ src/main/java/com/acc/cucumber_29jan/Test_runner.java
@@ -6,9 +6,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"json:target/cucumber.json"}
-		//features= "Feature03"
-		//,glue= {"step_definition03"}
+@CucumberOptions(plugin = {"json:target/cucumber.json"},
+		features= "Feature03"
+		,glue= {"step_definition03"}
 		
 		)
 
